2cc5287cf8649d8aad681cb2f8f1460e22f45089
soul10o01 <maher.alsayed,ahmad@gmail.com>
2025-06-11 20:30:59 +0300

    536

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/software-testing-pwa-2/',
   plugins: [react()],
   build: {
     assetsDir: 'assets',
